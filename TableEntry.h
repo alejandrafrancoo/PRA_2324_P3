820487ef166301413f1efef42b3772f1234bf423
Alejandra <alejandra.francovicente@gmail.com>
2025-12-18 16:15:56 +0100

    Actualizado varias cosas habian algunos problemas

--- TableEntry.h
+++ TableEntry.h
@@ -7,26 +7,23 @@
 template <typename V>
 class TableEntry {
 public:
-    // Atributos públicos
     std::string key;
     V value;
 
-    // Constructores
-    TableEntry(std::string key, V value) : key(key), value(value) {}
+    TableEntry() : key(""), value() {}
 
     TableEntry(std::string key) : key(key), value() {}
 
-    TableEntry() : key(""), value() {}
+    TableEntry(std::string key, V value) : key(key), value(value) {}
 
-    // Operadores de comparación (solo comparan la clave)
-    friend bool operator==(const TableEntry<V> &te1, const TableEntry<V> &te2) {
+    // Igualdad SOLO por clave
+    friend bool operator==(const TableEntry<V>& te1, const TableEntry<V>& te2) {
         return te1.key == te2.key;
     }
 
-    friend bool operator!=(const TableEntry<V> &te1, const TableEntry<V> &te2) {
+    friend bool operator!=(const TableEntry<V>& te1, const TableEntry<V>& te2) {
         return te1.key != te2.key;
     }
-
 
     // Ordenación lexicográfica por clave (OBLIGATORIO PARA ABB)
     friend bool operator<(const TableEntry<V>& te1, const TableEntry<V>& te2) {
@@ -37,13 +34,11 @@ public:
         return te1.key > te2.key;
     }
 
-
-    // Operador de salida
-    friend std::ostream& operator<<(std::ostream &out, const TableEntry<V> &te) {
-        out << "(" << te.key << " -> " << te.value << ")";
+    // Impresión
+    friend std::ostream& operator<<(std::ostream& out, const TableEntry<V>& te) {
+        out << "(" << te.key << ", " << te.value << ")";
         return out;
     }
 };
 
 #endif
-
